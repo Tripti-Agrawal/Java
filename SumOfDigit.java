f99b0468603a7223eb7ef640e54bd220c33dd116
Tripti-Agrawal <atripti379@gmail.com>
2026-04-25 17:30:04 +0530

    Add files via upload

---
+++ SumOfDigit.java
@@ -0,0 +1,12 @@
+public class SumOfDigit{
+    public static void main(String[] args) {
+        int num=12345;
+        int sum=0;
+        while(num!=0){
+            int lastDigit=num%10;
+            sum+=lastDigit;
+            num/=10;
+        }
+        System.out.println(sum);
+    }
+}
