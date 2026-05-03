e85fdd2248622ed9b1ccd32e143c98997451a9f2
Tripti-Agrawal <atripti379@gmail.com>
2026-05-03 23:46:03 +0530

    Add files via upload

---
+++ GCD.java
@@ -0,0 +1,31 @@
+public class GCD {
+    public static void main(String[] args) {
+        int a=24;
+        int b=36;
+
+        //Approach1
+        int min=a>b?b:a;
+        while(min>0){
+            if(a%min==0 && b%min==0){
+                System.out.println("GCD:"+min);
+                break;
+            }
+            min--;
+        } 
+
+        //Approach2
+        while(a%b!=0){
+            int rem=a%b;
+            a=b;
+            b=rem;
+        }
+        System.out.println("GCD:"+ b);
+
+        //Approch3
+        while(a!=b){
+            if(a>b) a-=b;
+            else b-=a;
+        }
+        System.out.println("GCD:"+a);
+    }
+}
